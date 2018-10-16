838917d7c37612040e2c143b3827d658bc0397f0
ccapetola20 <42975602+ccapetola20@users.noreply.github.com>
2018-10-15 20:11:41 -0400

    Add files via upload

---
+++ src/QuadraticFormula.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class QuadraticFormula 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner user = new Scanner(System.in);
+		System.out.print("Welcome to the Quadratic Formula Calculator!\nPress Enter to continue.");
+		user.nextLine();
+		System.out.println("QUADRATIC FORMULA CALCULATOR: ");
+		System.out.print("Please enter coefficient \"a\": ");
+		String aStr = user.nextLine();
+		double aDoub = Double.parseDouble(aStr);
+		System.out.print("\nPlease enter coefficient \"b\": ");
+		String bStr = user.nextLine();
+		double bDoub = Double.parseDouble(bStr);
+		System.out.print("\nPlease enter the value \"c\": ");
+		String cStr = user.nextLine();
+		double cDoub = Double.parseDouble(cStr);
+		double root = Math.sqrt(bDoub*bDoub - 4*aDoub*cDoub);
+		double solutionOne = ((-1*bDoub+root)/2*aDoub);
+		double solutionTwo = ((-1*bDoub-root)/2*aDoub);
+		System.out.print("\nThe solutions are: " + solutionOne + ", " + solutionTwo);
+		
+		
+
+	}
+
+}
